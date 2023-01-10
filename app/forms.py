c409d0f268b7695f58a01df4d828b5d830e15ed4
MHines10 <cd.mhines@gmail.com>
2023-01-10 15:04:23 -0500

    commit 1

---
+++ app/forms.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import EqualTo, InputRequired
---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    fruits = ['apple', 'banana', 'orange', 'strawberry', 'watermelon', 'mango', 'blueberry']
+    return render_template('index.html', name = 'Monti', fruits = fruits)
+    
+
+@app.route('/posts')
+def posts():
+    return 'These are the posts'
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
