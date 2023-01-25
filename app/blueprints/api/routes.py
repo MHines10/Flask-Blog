4b4f426bd3c53c2ecff9c7bc7d0cdb64d37c0fb5
MHines10 <cd.mhines@gmail.com>
2023-01-25 18:29:27 -0500

    Updated API to create new user and get new user ID

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,10 @@
 from flask import request
 from . import api
-from app.models import Post
+from app.models import Post, User
+
+#####################
+#        Post       #
+#####################
 
 @api.route('/')
 def index():
@@ -31,13 +35,56 @@ def create_post():
     # Check to see that the data is valid
     for field in ['title', 'body', 'user_id']:
         if field not in data:
+             # If the field is not in the request body, throw an error saying they are missing that field
             return {"error": "Missing field: " + field}, 400
     
-
+    # pull the fields from the request data
     title = data.get('title')
     body = data.get('body')
     user_id = data.get('user_id')
 
+     # Create a new Post instance with data from request
     new_post = Post(title=title, body=body, user_id=user_id)
 
+    # Return the new post as a JSON response
     return new_post.to_dict(), 201
+
+
+#####################
+#        User       #
+#####################
+
+# Endpoint to create a new user
+@api.route('/users', methods=['POST'])
+def create_user():
+    # Check to see that the request sent a JSON request body
+    if not request.is_json:
+        return {"error": "Missing JSON in request"}, 400
+    # Get the data from the request body
+    data = request.json
+
+    # Check to see that the data is valid
+    for field in ['email', 'username', 'password']:
+        if field not in data:
+            return {f"error: Missing field: ${field} "}, 400
+
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+
+    new_user = User(email=email, username=username, password=password)
+    new_user.save()
+
+    return new_user.to_dict(), 201
+
+# Endpoint to get an existing user
+@api.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return user.to_dict(), 200
+ 
+# Endpoint to get all users
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return [user.to_dict() for user in users], 200
