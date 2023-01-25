f8d9d34c123f0d0137c76c6f3d66758b18e8ae49
MHines10 <cd.mhines@gmail.com>
2023-01-25 17:01:02 -0500

    flask CORS

---
+++ app/blueprints/api/routes.py
@@ -0,0 +1,18 @@
+from . import api
+from app.models import Post
+
+@api.route('/')
+def index():
+    return 'Hello this is the api'
+
+# Endpoint to get ALL of the posts
+@api.route('/posts')
+def get_posts():
+    posts = Post.query.all()
+    return [p.to_dict() for p in posts]
+
+# Endpoint to  get a single post
+@api.route('/posts/<int:post_id>')
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return post.to_dict()
--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, EmailField, TextAreaField
 from wtforms.validators import EqualTo, InputRequired
 
 
@@ -8,4 +8,16 @@ class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     confirm_pass = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField()
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    body = TextAreaField('Body', validators=[InputRequired()])
     submit = SubmitField()
